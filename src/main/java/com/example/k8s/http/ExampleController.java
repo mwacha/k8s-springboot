48393114fd7bbd2518d09ed6b04562f807b99083
mwacha <mwacha@gmail.com>
2022-03-15 22:29:50 -0300

    chore:  add readinessProbe, livenessProbe and startupProbe:

--- src/main/java/com/example/k8s/http/ExampleController.java
+++ src/main/java/com/example/k8s/http/ExampleController.java
@@ -5,12 +5,20 @@ import com.example.k8s.config.ParamProperties;
 import com.example.k8s.service.ConfigMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.http.HttpResponse;
 import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping("/")
@@ -19,6 +27,21 @@ public class ExampleController {
 
     private final ParamProperties properties;
     private final ConfigMap configMap;
+    private LocalDateTime startedAt = LocalDateTime.now();
+
+
+    @GetMapping(path = "/healthz")
+    public String getHealthz(HttpServletResponse response) {
+
+        var duration = Duration.between(startedAt, LocalDateTime.now());
+
+        if(duration.getSeconds() < 10 || duration.getSeconds() > 30) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return MessageFormat.format("<h1>Duration: {0}</h1>", duration.getSeconds());
+        }
+        response.setStatus(HttpStatus.OK.value());
+        return "OK";
+    }
 
     @GetMapping(path = "/secret")
     public String getSecret() {
